5cc24dc774cedb57a1573173534a49b82ac18946
gelinas <jlgelinas@gmail.com>
2020-01-04 00:22:47 -0500

    document utility functions

--- src/utils/ScrollToTop.js
+++ src/utils/ScrollToTop.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+// fix undesired behavior in react-router 
+// resolves issue where route navigation does not return you to the top of the page
+
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+// return an instance of axios with the API base url and token for performing HTTP requests
+
 export const axiosWithAuth = () => {
     const token = "fe2a48bec17b3cc81cdb99a6836a62a1bbfb4e89dd5ba073bbcd284dd93138c0";
-    // return an instance of axios
     return axios.create({
         baseURL: 'https://frontendtest.rivet.work/api/v1/',
         headers: {
